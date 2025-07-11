cc07f83d7137028bdfa990682e9a3ee9fba95bd5
Minkyu Cho <minkyu@greyboxhq.com>
2025-07-11 19:49:05 +0900

    feat: update

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,11 @@
+import Clix from '../index';
+
+describe('Clix SDK', () => {
+  it('should export Clix module', () => {
+    expect(Clix).toBeDefined();
+  });
+
+  it('should have initialize method', () => {
+    expect(typeof Clix.initialize).toBe('function');
+  });
+}); 
